4ce35f888877f94ffac6b4d858526e5ba90da9a8
Alvaro-21 <66224069+Alvaro-21@users.noreply.github.com>
2020-05-31 23:45:53 +0200

    Initial commit

---
+++ src/Juego/Broncas.java
@@ -0,0 +1,16 @@
+package Juego;
+import javax.swing.JPanel;
+public class Broncas  extends Personaje{
+private String Sentido;
+Broncas(JPanel MiJ,int x, int y){
+	super(MiJ);
+	this.X=x;
+	this.Y=y;
+	this.Sentido="ESTE";
+}
+public void movimientoBroncas() {
+	if(this.X<0) {
+		
+	}
+}
+}
---
+++ src/Juego/Personaje.java
@@ -0,0 +1,42 @@
+package Juego;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Personaje {
+	protected int X;
+	protected int Y;
+	private String Direccion;
+	
+	private JPanel MiJP;
+
+	Personaje(JPanel MiJ) {
+		this.X = 0;
+		this.Y = 0;
+		this.MiJP = MiJ;
+	}
+
+	/*public void DibujarPersonaje(Graphics g, int X, int Y, String Direccion) {
+		this.X = X;
+		this.Y = Y;
+		this.MiJP.update(g);
+		ImageIcon MiImagen = new ImageIcon(Direccion);
+		g.drawImage(MiImagen.getImage(), X, Y, MiJP);
+	}*/
+	
+	public int Coordx() {
+		return this.X;
+	}
+	public int Coordy() {
+		return this.Y;
+	}
+public String getDireccion() {return this.Direccion;}
+public void setDireccion(String Direccion) {this.Direccion=Direccion;}
+public void setCoordX(int x) {this.X=x;}
+public void setCoordY(int y) {this.Y=y;}
+public JPanel getPanel() {return this.MiJP;}
+
+
+
+}
